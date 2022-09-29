8b16118305148ca20efd57b2407bb8c35f5d21c7
vinybergamo <vinybergamo@gmail.com>
2022-09-29 13:43:19 -0300

    Make Auth Controller

---
+++ server/app/Controllers/Http/AuthController.ts
@@ -0,0 +1,11 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class AuthController {
+  public async register() {}
+
+  public async login() {}
+
+  public async auth() {}
+
+  public async logout() {}
+}
