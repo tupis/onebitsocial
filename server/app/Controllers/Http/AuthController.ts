4d1598d79f1f6cbe39c7bbc58745efa914d3237c
vinybergamo <vinybergamo@gmail.com>
2022-09-29 14:00:01 -0300

    User Register System implemented

--- server/app/Controllers/Http/AuthController.ts
+++ server/app/Controllers/Http/AuthController.ts
@@ -1,7 +1,31 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import User from 'App/Models/User'
 
 export default class AuthController {
-  public async register() {}
+  public async register({ request, response }: HttpContextContract) {
+    const userSchema = schema.create({
+      name: schema.string({}, [rules.maxLength(50)]),
+      username: schema.string({ trim: true }, [
+        rules.unique({ table: 'users', column: 'username', caseInsensitive: true }),
+        rules.maxLength(30),
+      ]),
+      email: schema.string({ trim: true }, [
+        rules.email(),
+        rules.unique({ table: 'users', column: 'email', caseInsensitive: true }),
+        rules.maxLength(255),
+      ]),
+      password: schema.string({}, [rules.minLength(8)]),
+    })
+
+    const data = await request.validate({ schema: userSchema })
+
+    const user = await User.create(data)
+
+    response.status(201)
+
+    return user
+  }
 
   public async login() {}
 
