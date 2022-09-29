1d034617dc2d1792853a1a6e7c4522e9e08d769f
vinybergamo <vinybergamo@gmail.com>
2022-09-29 14:23:40 -0300

    Users management routes create

---
+++ server/app/Controllers/Http/UsersController.ts
@@ -0,0 +1,11 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class UsersController {
+  public async index() {}
+
+  public async show() {}
+
+  public async update() {}
+
+  public async delete() {}
+}
