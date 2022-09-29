b7d620d5c9478423f3750284184fdd352351a109
vinybergamo <vinybergamo@gmail.com>
2022-09-29 14:26:38 -0300

    List all user implemented

--- server/app/Controllers/Http/UsersController.ts
+++ server/app/Controllers/Http/UsersController.ts
@@ -1,7 +1,12 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
 
 export default class UsersController {
-  public async index() {}
+  public async index() {
+    const users = await User.all()
+
+    return users
+  }
 
   public async show() {}
 
