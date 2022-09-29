298c8b0c343413240fdd5b58dbd19794e2c31ca5
vinybergamo <vinybergamo@gmail.com>
2022-09-29 13:40:10 -0300

    Configure Migrations

--- server/database/migrations/1664468557002_users.ts
+++ server/database/migrations/1664468557002_users.ts
@@ -6,7 +6,10 @@ export default class UsersSchema extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
-      table.string('email', 255).notNullable()
+
+      table.string('name', 50).notNullable()
+      table.string('username', 30).notNullable().unique()
+      table.string('email', 255).notNullable().unique()
       table.string('password', 180).notNullable()
       table.string('remember_me_token').nullable()
 
