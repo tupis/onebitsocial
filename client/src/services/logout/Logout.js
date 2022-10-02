fc359dec6f766766d31b9a4aaa3d92c859c30e36
vinybergamo <vinybergamo@gmail.com>
2022-10-02 10:12:15 -0300

    Feat: Logout system implemented

---
+++ client/src/services/logout/Logout.js
@@ -0,0 +1,19 @@
+import Api from "../Api";
+import GetCookies from "../cookies/getCookie";
+import removeCookie from "../cookies/removeTokenCookie";
+
+const Logout = async () => {
+  await Api.post(
+    "/user/logout",
+    {},
+    {
+      headers: {
+        Authorization: `Bearer ${GetCookies(import.meta.env.VITE_API_URL)}`,
+      },
+    }
+  ).then((res) => console.log(res.data));
+  removeCookie(import.meta.env.VITE_API_URL, true);
+  window.location.href = "/login";
+};
+
+export default Logout;
