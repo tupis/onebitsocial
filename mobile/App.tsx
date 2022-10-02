e579eb51dcac6bd2fccf83f433904898b9e7c199
Tupis <joaoh.tupinamba@gmail.com>
2022-10-02 00:05:01 -0300

    fix: use route and ui kitten

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,22 +2,18 @@ import * as eva from "@eva-design/eva";
 import { ApplicationProvider } from "@ui-kitten/components";
 import Routes from "./routes";
 import { useColorScheme } from "react-native";
-import Login from "./src/screens/Login";
-import Register from "./src/screens/Register";
 
 const App = () => {
   const theme = useColorScheme();
 
-  // return (
-  //   <ApplicationProvider
-  //     {...eva}
-  //     theme={theme === "dark" ? eva.dark : eva.light}
-  //   >
-  //     <Routes />
-  //   </ApplicationProvider>
-  // );
-
-  return <Register />;
+  return (
+    <ApplicationProvider
+      {...eva}
+      theme={theme === "dark" ? eva.dark : eva.light}
+    >
+      <Routes />
+    </ApplicationProvider>
+  );
 };
 
 export default App;
