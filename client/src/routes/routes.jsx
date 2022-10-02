7354fc49754271cbe81708f75025490893828967
Guilherme Castro <guii_1@hotmail.com>
2022-10-01 21:31:34 -0300

    feat: create pages login/register

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -4,6 +4,8 @@ import Dashboard from "../screens/Dashboard";
 import Login from "../screens/Login";
 import Redirect from "../screens/Redirect";
 import PrivateRoute from "./Privates";
+import Register from "../screens/Register";
+import Error404 from '../screens/Error404'
 
 function App() {
   const Nekot = localStorage.getItem("Nekot");
@@ -19,7 +21,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/redirect" element={<Redirect />} />
+          <Route path="/error" element={ <Error404/>} />
           <Route element={<PrivateRoute isLogged={auth} />}>
             <Route path="/dashboard" element={<Dashboard />} />
           </Route>
